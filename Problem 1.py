3b8e1e908e6533a76ea677ee8440eef239069eaa
Shradha Gaikwad <g.shradha@gmail.com>
2014-03-08 09:03:20 -0500

    find sum of all multiples of 3 and 5 from a range

---
+++ Problem 1.py
@@ -0,0 +1,16 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+def func(number):
+    result=[]
+    for x in range(number):
+        if x % 3== 0 or x % 5==0:
+            result.append(x)
+    return sum(result)
+    
+    
+
+
+print func(1000)
